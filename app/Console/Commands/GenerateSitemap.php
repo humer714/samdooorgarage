17adb53c27e76cda52d38a5982bb5b8a6c0cca07
Umer <119045470+humer714@users.noreply.github.com>
2025-01-19 23:28:52 +0500

    Update Changes

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -60,6 +60,24 @@ class GenerateSitemap extends Command
         // Save the sitemap to a file (public folder, for example)
         $sitemap->writeToFile(public_path('sitemap.xml'));
 
+        // Remove unnecessary tags from the sitemap
+        $this->removeUnnecessaryTags(public_path('sitemap.xml'));
+
+        
+
         $this->info('Sitemap generated successfully!');
+    }
+
+    protected function removeUnnecessaryTags($filePath)
+    {
+        // Get the content of the sitemap
+        $sitemapContent = file_get_contents($filePath);
+
+        // Remove <changefreq> and <priority> tags
+        $sitemapContent = preg_replace('/<changefreq>.*?<\/changefreq>/', '', $sitemapContent);
+        $sitemapContent = preg_replace('/<priority>.*?<\/priority>/', '', $sitemapContent);
+
+        // Write the modified content back to the file
+        file_put_contents($filePath, $sitemapContent);
     }
 }
